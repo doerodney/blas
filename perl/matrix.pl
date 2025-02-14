c3dff89aa49a243bdf79b1a04bb7b42152535af6
Rod Doe <doerodney@gmail.com>
2025-02-14 14:59:09 -0800

    Interim commit - helper functions complete.

--- perl/matrix.pl
+++ perl/matrix.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl
+
 use strict;
 
 my $NROWS = 3;
@@ -19,8 +21,28 @@ sub matrix_new {
 }
 
 
-sub matrix_elem_get
-{
+sub matrix_add_row_to_row {
+  my ($rm, $src_row, $dest_row) = @_;
+  
+  my $ncols = matrix_ncols($rm);
+
+  for (my $col = 0; $col < $ncols; $col++) {
+    my $src_val = matrix_elem_get($rm, $src_row, $col);
+    my $dest_val = matrix_elem_get($rm, $dest_row, $col);
+    matrix_elem_set($rm, $dest_row, $col, ($src_val + $dest_val));
+  }
+}
+
+
+sub matrix_clean {
+  my $rm = shift;
+
+  # Test that pivot values are non-zero. 
+  # If found, find a row whose value in the pivot column is non-zero, 
+  # and add the row to the pivot row.
+}  
+
+sub matrix_elem_get {
   my ($rm, $row, $col) = @_;
   my $idx = matrix_index($rm, $row, $col);
   my $rdata = $rm->{'rdata'};
@@ -29,8 +51,7 @@ sub matrix_elem_get
 }  
 
 
-sub matrix_elem_set
-{
+sub matrix_elem_set {
   my ($rm, $row, $col, $value) = @_;
   my $idx = matrix_index($rm, $row, $col);
   my $rdata = $rm->{'rdata'};
@@ -50,11 +71,27 @@ sub matrix_index {
 
 
 sub matrix_interchange_rows {
-  my ($rm, $a, $b) = @_;
+  my ($rm, $rowa, $rowb) = @_;
 
   my $ncols = matrix_ncols($rm);
   my @buf = (0) * $ncols;
 
+  # Copy row a to buf:
+  for (my $col = 0; $col < $ncols; $col++) {
+    my $value = matrix_elem_get($rm, $rowa, $col);
+    push(@buf, $value);
+  }
+
+  # Copy row b to row a:
+  for (my $col = 0; $col < $ncols; $col++) {
+    my $value = matrix_elem_get($rm, $rowb, $col);
+    matrix_elem_set($rm, $rowa, $col);
+  }
+
+  # Copy buf to row b:
+  for (my $col = 0; $col < $ncols; $col++) {
+    matrix_elem_set($rm, $rowb, $col, $buf[$col]);
+  }
 }
 
 
@@ -78,6 +115,8 @@ sub matrix_print {
   my $space = '  ';
   my $str = '';
 
+  print "\n---\n";
+
   for (my $row = 0; $row < $nrows; $row++) {
     $str = '';
     for (my $col = 0; $col < $ncols; $col++) {
@@ -86,32 +125,53 @@ sub matrix_print {
     }
     print "$str\n";
   }
+
+  print "---\n";
 }
 
 
+# This is not correct!
 sub matrix_reduce {
   my $rm = shift;
   my $nrows = matrix_nrows($rm);
   my $ncols = matrix_ncols($rm);
- 
+
   matrix_print($rm);
 
-  for (my $pivot = 0; $pivot < $nrows; $pivot++) {
-    my $next = $pivot + 1;
-    # START HERE
-  }
+  # Start at the bottom row, and convert values to the left of the pivot to 0.
+  for (my $pivot = ($nrows - 1); $pivot >= 1; $pivot--) {
+    my $prev = $pivot - 1;
+    for (my $col = 0; $col < $pivot; $col++) {
+      # Scale the current row to have a left value of 1.
+      # Get the value in the left column: 
+      my $value = matrix_elem_get($rm, $pivot, $col);
+      if ($value != 0) {
+        # Scale the row to have -1 in the left column:
+        matrix_scale_row($rm, $pivot, (-1/$value));
+      }
 
+      # Scale the previous row to have a left value of 1:
+      $value = matrix_elem_get($rm, $prev, $col);
+      if ($value != 0) {
+        # Add prev row to current row:
+        matrix_add_row_to_row($rm, $prev, $pivot);
+      }
+      matrix_print($rm);
+    }
+  }
 }
 
 
 sub matrix_scale_row {
   my ($rm, $row, $scale) = @_;
 
-  my $ncols = matrix_ncols($rm);
+  if ($scale != 0) {
+    my $ncols = matrix_ncols($rm);
 
-  for (my $col = 0; $col < $ncols; $col++) {
-    my $value = matrix_elem_get($rm, $row, $col);
-    matrix_elem_set($rm, $row, $col, ($value * $scale));
+    for (my $col = 0; $col < $ncols; $col++) {
+      my $value = matrix_elem_get($rm, $row, $col);
+      matrix_elem_set($rm, $row, $col, ($value * $scale));
+    }
   }
 }
 
@@ -149,3 +209,6 @@ matrix_elem_set($rm, 2, 2, 4);
 matrix_elem_set($rm, 2, 3, 39);
 
 matrix_print($rm);
+
+matrix_reduce($rm);
+
